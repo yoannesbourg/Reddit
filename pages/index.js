c9d639b2ec221a89a13719e85d2aa3a44288a00d
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 18:17:31 +0100

    first design setup

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 import Head from 'next/head'
 import Searchbar from '../components/Searchbar'
+import Post from '../components/Post'
+import Categories from '../components/Categories'
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-red-500">
+    <div className="min-h-screen bg-gray-50">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
+      <main className="text-gray-500">
         <Searchbar />
+        <div className="flex mx-auto -my-0 w-11/12">
+          <div className=" flex w-8/12 flex-col  mr-4">
+            <Post />
+            <Post />
+          </div>
+          <div className=" flex justify-center w-4/12">
+            <Categories />
+          </div>
+        </div>
       </main>
-
       {/* <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
