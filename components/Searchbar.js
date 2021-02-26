11ad351bf013ea02e1cfe7afcd774ae26120c6da
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 18:17:07 +0100

    Create Searchbar component

--- components/Searchbar.js
+++ components/Searchbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const Searchbar = () => {
-    return (
-        <div className="bg-white w-screen flex justify-center p-3">
-            <input placeholder="Search a post" className="bg-gray-50 p-1.5 w-72"/>
+    return (      
+
+        <div className="bg-white flex justify-center p-3 shadow">
+            {/* <input placeholder="Search a post" className="bg-gray-50 p-2 w-72 px-5 rrounded-lg"/> */}
+            <input 
+                className="border-2 border-gray-300 bg-white h-10 px-5 pr16 rounded-lg text-sm focus:outline-none"
+                type="search" 
+                name="search"
+                placeholder="Search"
+            />
         </div>
     )
 }
