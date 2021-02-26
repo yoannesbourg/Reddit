e9b84278c38155728f181ed7e3e97a2e3ab58125
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 15:51:41 +0100

    Create Searchbar

---
+++ components/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Searchbar = () => {
+    return (
+        <div className="bg-white w-screen flex justify-center p-3">
+            <input placeholder="Search a post" className="bg-gray-50 p-1.5 w-72"/>
+        </div>
+    )
+}
+
+export default Searchbar
