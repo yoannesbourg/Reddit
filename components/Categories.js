3a4c15bc211444b9d23a64dd25e3e5d91e26c5cc
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 18:16:32 +0100

    Create categories component

---
+++ components/Categories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div className="bg-white  flex p-3 w-40 shadow mt-4 w-full rounded-lg">
+            <ul>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+                <li>Categories</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Categories
