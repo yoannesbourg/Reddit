d9a3956c9fd637d17947b948673f423f3048c973
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 18:55:58 +0100

    Fetch data from reddit api

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,59 @@ import Head from 'next/head'
 import Searchbar from '../components/Searchbar'
 import Post from '../components/Post'
 import Categories from '../components/Categories'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://www.reddit.com/r/popular.json');
+      const json = await response.json();
+      const result = json.data.children.map((post) => post.data.title)
+      console.log(result)
+      return result
+    }
+    fetchData().then(result => setData(result));
+  }, []); 
+
   return (
     <div className="min-h-screen bg-gray-50">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className="text-gray-500">
-        <Searchbar />
-        <div className="flex mx-auto -my-0 w-11/12">
-          <div className=" flex w-8/12 flex-col  mr-4">
-            <Post />
-            <Post />
+        <Searchbar /> {/* Search top bar */}
+
+        <div className="flex mx-auto -my-0 w-11/12"> {/* Main content*/}
+
+          <div className=" flex w-8/12 flex-col  mr-4"> {/*Left Column*/}
+            
+          {data.map(item => <Post title={item.title} author={item.author}/>)}
+
           </div>
-          <div className=" flex justify-center w-4/12">
+
+          <div className=" flex justify-center w-4/12"> {/*Right Column*/}
             <Categories />
           </div>
+
         </div>
       </main>
-      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
+
+      <footer className="flex items-center justify-center w-full h-24 border-t mt-4">
         <a
           className="flex items-center justify-center"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by Yoann{' '}
+  
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
         </a>
-      </footer> */}
+      </footer>
     </div>
   )
 }
