08a7e65bafeaa90471ee499170c2f2dbdd6479f1
Yoann <yoannpascalbouix@gmail.com>
2021-02-26 18:56:23 +0100

    Pass data props to Post

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({title, author}) => {
     return (
         <div className="bg-white  flex-column  p-3 w-40 shadow mt-4 w-full rounded-lg">
-            <h2 className="text-lg font-bold">This is a post</h2>
+            <h2 className="text-lg font-bold">{title}</h2>
             <div className="flex justify-between">
-                <p>Author</p>
+                <p>{author}</p>
                 <p>Date</p>
             </div>
         </div>
